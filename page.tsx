b835c81bbce3cd69518d9defe711894dbf1ec2e5
iqraoffical <159756553+iqraoffical@users.noreply.github.com>
2024-11-13 09:24:16 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <div
+        className="text bg-lime-600   text-5xl font-extrabold ...
+      bg-clip-border md:bg-clip-padding text-align: center px-9 p-6  
+     t "
+      >
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+}
